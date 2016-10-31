2ba6a0677553cd410d542d1ea2a78a2ef7780617
almccombs <amccombs@iastate.edu>
2016-10-30 21:12:59 -0500

    update munging script

--- nectar analysis/munging scripts/Balsam15.R
+++ nectar analysis/munging scripts/Balsam15.R
@@ -3,13 +3,14 @@ library(tidyr)
 
 setwd("./nectar analysis/raw data")
 
-bals15 <- read.csv("2015 Balsamroot Nectar Data v1 original.csv", header = T, as.is = T)
+bals15 <- read.csv("2015 Balsamroot Nectar Data v1 original w tube length.csv", header = T, as.is = T)
 
 #get rid of wind, temp, humidity, shade/sun, time, and notes
 bals15 <- bals15[,-c(2,3,4,5,6,20)]
 
 bals15$date <- mdy(bals15$date)
 
+# Get the plots properly labelled
 plots <- as.data.frame(strsplit(bals15$plot, split = " "))
 plots <- t(plots)
 plots <- as.data.frame(plots)
@@ -29,3 +30,9 @@ bals15$plotno <- as.factor(plots$number)
 bals15$plotname <- as.factor(plots$name)
 bals15$treatment <- as.factor(plots$treatment)
 bals15 <- bals15[,c(1,2,16,15,3,4,5,6,7,8,9,10,11,12,13,14)]
+
+# calculate volume
+vols <- as.data.frame(bals15[,c("cm.of.tube.filled", "cm.of.second.tube.filled", "cm.of.third.tube", "cm.of.fourth.tube", "length.tube..mm.")])
+vols$totalvol <- (rowSums(vols[1:4], na.rm = T))*10
+
+bals15$Plant <- as.factor(bals15$Plant)
