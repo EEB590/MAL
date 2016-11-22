d60a5d8ce9bfd8654cbf45cf27aea06ba0da5936
almccombs <amccombs@iastate.edu>
2016-11-22 15:03:03 -0600

    fixed balsam 2016 munging script

--- nectar analysis/munging scripts/Balsam16.R
+++ nectar analysis/munging scripts/Balsam16.R
@@ -1,56 +1,30 @@
 library(lubridate)
 library(tidyr)
 
-buck16 <- read.csv("nectar analysis/raw data/2016 Buckwheat Nectar Data raw data FINAL.csv", header = T, as.is = T)
+bals16 <- read.csv("nectar analysis/raw data/2016 Balsamroot Nectar Data raw data FINAL corrected.csv", header = T, as.is = T)
 
 #get rid of empty rows, plus wind, temp, humidity, shade/sun, time, and notes
-buck16 <- subset(buck16[!is.na(buck16$plot),])
-buck16 <- subset(buck16, select = -c(avg.wind..km.h., temp...C., Humidity, Shade..Sun, time..am., Volume..µL., notes, X, X.1))
+bals16 <- subset(bals16[!is.na(bals16$Plant..),])
+bals16 <- subset(bals16, select = -c(avg.wind..mph., temp...F., Humidity...., Shade..Sun, time..am., Volume..µL., notes))
 
-buck16$date <- mdy(buck16$date)
+bals16$date <- mdy(bals16$date)
 
 # Get the plots properly labelled
+bals16$treatment <- as.factor(gsub("[[:digit:]]", "", bals16$plot))
+bals16$treatment <- gsub("^C", "", bals16$treatment)
+bals16$treatment <- gsub("W", "", bals16$treatment)
+bals16$treatment <- gsub("E", "", bals16$treatment)
+bals16$treatment <- gsub("HSR", "H", bals16$treatment)
+bals16$treatment <- gsub("SR", "C", bals16$treatment)
 
-#Note - plot labels on data sheets were numbers only (not location or treatment).  Using Diane's numbering system (not Matt's numbering system) to identify plots and treatment.
-
-buck16$treatment <- buck16$plot
-buck16$treatment <- gsub("12","H", buck16$treatment)
-buck16$treatment <- gsub("11","C", buck16$treatment)
-buck16$treatment <- gsub("10","C", buck16$treatment)
-buck16$treatment <- gsub("9","H", buck16$treatment)
-buck16$treatment <- gsub("8","H", buck16$treatment)
-buck16$treatment <- gsub("7","C", buck16$treatment)
-buck16$treatment <- gsub("6","C", buck16$treatment)
-buck16$treatment <- gsub("5","H", buck16$treatment)
-buck16$treatment <- gsub("4","H", buck16$treatment)
-buck16$treatment <- gsub("3","C", buck16$treatment)
-buck16$treatment <- gsub("2","C", buck16$treatment)
-buck16$treatment <- gsub("1","H", buck16$treatment)
-buck16$treatment <- as.factor(buck16$treatment)
-
-buck16$plotna <- buck16$plot
-buck16$plotna <- gsub("12", "WH", buck16$plotna)
-buck16$plotna <- gsub("11", "WC", buck16$plotna)
-buck16$plotna <- gsub("10", "WSR", buck16$plotna)
-buck16$plotna <- gsub("9", "WHSR", buck16$plotna)
-buck16$plotna <- gsub("8", "CHSR", buck16$plotna)
-buck16$plotna <- gsub("7", "CSR", buck16$plotna)
-buck16$plotna <- gsub("6", "CC", buck16$plotna)
-buck16$plotna <- gsub("5", "CH", buck16$plotna)
-buck16$plotna <- gsub("4", "EH", buck16$plotna)
-buck16$plotna <- gsub("3", "EC", buck16$plotna)
-buck16$plotna <- gsub("2", "ESR", buck16$plotna)
-buck16$plotna <- gsub("1", "EHSR", buck16$plotna)
-
-buck16$plot <- paste(buck16$plotna, buck16$plot, sep = "")
-buck16$plot <- as.factor(buck16$plot)
-buck16 <- subset(buck16, select = -plotna)
-
-
+bals16$plant <- as.factor(paste(bals16$plot,bals16$Plant..,sep = "-"))
+bals16 <- subset(bals16, select = -Plant..)
+bals16$plot <- as.factor(bals16$plot)
+bals16$treatment <- as.factor(bals16$treatment)
 
 # calculate volume
 bals16$volume <- (bals16$mm.of.tube.filled/55)/bals16$size.tube..µL.
-    #all tubes used were 55mm in length (see scanned data sheets)
+#all tubes used were 55mm in length (see scanned data sheets)
 bals16 <- subset(bals16, select = -c(mm.of.tube.filled, size.tube..µL.))
 
 #calculate sugar mass
