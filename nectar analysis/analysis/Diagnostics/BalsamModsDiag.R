3f5b2cb639b085051f5ea8610b0ebaf04f0c3b0a
almccombs <amccombs@iastate.edu>
2016-11-28 17:31:10 -0600

    update diagnostics for model with plot/plant nested random effects

--- nectar analysis/analysis/Diagnostics/BalsamModsDiag.R
+++ nectar analysis/analysis/Diagnostics/BalsamModsDiag.R
@@ -4,7 +4,7 @@ library(nlme)
 library(lubridate)
 library(influence.ME)
 
-#setwd("D:/Iowa State University/Debinski Lab/Nectar data/MAL") only need this to knit
+setwd("D:/Iowa State University/Debinski Lab/Nectar data/MAL")
 
 balsvol15 <- read.csv("nectar analysis/data files/balsvol15.csv", header = T)
 balsvol16 <- read.csv("nectar analysis/data files/balsvol16.csv", header = T)
@@ -16,17 +16,17 @@ balssug16 <- read.csv("nectar analysis/data files/balssugar16.csv", header = T)
 balssugboth <- rbind(balssug15,balssug16)
 balssugboth$year <- as.factor(year(balssugboth$date))
 
-modvol <- lmer(volume ~ treatment * year + (1|plant), data = balsvolboth)
+modvol <- lmer(volume ~ treatment * year + (1|plot/plant), data = balsvolboth)
 plot(modvol, main = "Balsam Volume")
 inflvol <- influence(modvol, obs = T)
 plot(inflvol, which = "cook", main = "Balsam Volume")
 
-modBRIX <- lmer(BRIX ~ treatment * year + (1|plant), data = balssugboth)
+modBRIX <- lmer(BRIX ~ treatment * year + (1|plot/plant), data = balssugboth)
 plot(modBRIX, main = "Balsam BRIX")
 inflBRIX <- influence(modBRIX, obs = T)
 plot(inflBRIX, which = "cook", main = "Balsam BRIX")
 
-modmass <- lmer(mass ~ treatment * year + (1|plant), data = balssugboth)
+modmass <- lmer(mass ~ treatment * year + (1|plot/plant), data = balssugboth)
 plot(modmass, main = "Balsam mass")
 inflmass <- influence(modmass, obs = T)
 plot(inflmass, which = "cook", main = "Balsam mass")
