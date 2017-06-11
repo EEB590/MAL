03a89ce92da908c27e368b3a2f663a1c65b6ee22
almccombs <amccombs@iastate.edu>
2017-06-10 22:37:34 -0500

    Add plots of date and quad versus volume

--- nectar analysis/analysis/Data Exploration/BuckVolExplr.R
+++ nectar analysis/analysis/Data Exploration/BuckVolExplr.R
@@ -56,3 +56,7 @@ qqnorm(buckvol15$volume)
 qqnorm(buckvol16$volume)
 qqnorm(buckvolboth$volume)
 
+#Checking spread of key variables
+with(buckvolboth, plot(quad, vol))
+with(buckvolboth, plot(date, vol))
+
