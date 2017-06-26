6740ebde5913db32e23eb1df682f4b5dc0f2afcb
almccombs <amccombs@iastate.edu>
2017-06-26 18:56:53 -0500

    Update balsamroot volume model to use ln(volume) because need to transform for better model fit.  Include model diagnostics in this file.

--- nectar analysis/analysis/Balsamroot models/ModBalsVolBoth.R
+++ nectar analysis/analysis/Balsamroot models/ModBalsVolBoth.R
@@ -11,7 +11,10 @@ setwd("D:/Iowa State University/Debinski Lab/Nectar data/MAL")
 balsvol15 <- read.csv("nectar analysis/data files/balsvol15.csv", header = T)
 balsvol16 <- read.csv("nectar analysis/data files/balsvol16.csv", header = T)
 balsvolboth <- rbind(balsvol15,balsvol16)
+rm(balsvol15)
+rm(balsvol16)
 
+balsvolboth$lnvol <- log(balsvolboth$volume)
 balsvolboth$year <- as.factor(year(balsvolboth$date))
 
 cellN <- with(balsvolboth, table(treatment, year))
@@ -20,31 +23,27 @@ cellN
 cellMean <- with(balsvolboth, tapply(volume, list(treatment, year), mean))
 cellMean
 
-modvol <- lmer(volume ~ treatment * year + (1|plot/plant) + (1|year:date), data = balsvolboth)
-summary(modvol)
+# Model ln(volume), hypothesis test
+  #modeled ln of volume because using volume itself doesn't give us good residuals
+modlnvol <- lmer(lnvol ~ treatment * year + (1|plot/plant) + (1|year:date), data = balsvolboth)
+summary(modlnvol)
+plot(modlnvol)
+inflvol <- influence(modlnvol, obs = T)
+plot(inflvol, which = "cook", main = "Balsam Volume")
 
-volume.grid <- ref.grid(modvol)
-summary(volume.grid)
+lnvol.grid <- ref.grid(modlnvol)
+summary(lnvol.grid)
 
-lsmeans(volume.grid, "treatment")
-lsmeans(volume.grid, "year")
+lnvol.treat <- lsmeans(lnvol.grid, "treatment")
+pairs(lnvol.treat)
 
-volume.treat <- lsmeans(volume.grid, "treatment")
-pairs(volume.treat)
-pairs.treat <- pairs(volume.treat)
-test(pairs.treat, joint = T)
+lnvol.year <- lsmeans(lnvol.grid, "year")
+pairs(lnvol.year)
 
-volume.year <- lsmeans(volume.grid, "year")
-pairs(volume.year)
-pairs.year <- pairs(volume.year)
-test(pairs.year, joint = T)
-
-int.vol <- pairs(volume.grid, by = "year")
-int.vol
+int.vol <- pairs(lnvol.grid, by = "year")
 int.voltable <- update(int.vol, by = NULL)
 int.voltable
 
 test(pairs(int.voltable), joint = T)
 
-Anova(modvol, type = 3)
-
+Anova(modlnvol, type = 3)
