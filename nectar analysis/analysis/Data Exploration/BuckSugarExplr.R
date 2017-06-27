4e9b1a96b8883baffab9512ab387ab15b0ae41c8
almccombs <amccombs@iastate.edu>
2017-06-26 20:25:27 -0500

    Update buckwheat sugar data explore to include ln(mass)

--- nectar analysis/analysis/Data Exploration/BuckSugarExplr.R
+++ nectar analysis/analysis/Data Exploration/BuckSugarExplr.R
@@ -7,6 +7,10 @@ bucksug15 <- read.csv("nectar analysis/data files/bucksugar15.csv", header = T)
 bucksug16 <- read.csv("nectar analysis/data files/bucksugar16.csv", header = T)
 bucksugboth <- rbind(bucksug15,bucksug16)
 
+bucksug15$lnmass <- log(bucksug15$mass)
+bucksug16$lnmass <- log(bucksug16$mass)
+bucksugboth$lnmass <- log(bucksugboth$mass)
+
 #Data summaries
 
 summary(bucksug15)
@@ -28,6 +32,10 @@ qplot(bucksugboth$BRIX, binwidth = 1)
 qplot(bucksug15$mass, binwidth = 0.005)
 qplot(bucksug16$mass, binwidth = .0025)
 qplot(bucksugboth$mass, binwidth = .0025)
+
+qplot(bucksug15$lnmass, binwidth = 0.05)
+qplot(bucksug16$lnmass, binwidth = .05)
+qplot(bucksugboth$lnmass, binwidth = .05)
 
 
 ggplot(bucksug15, aes(x=treatment, y=BRIX)) + geom_boxplot() +
@@ -76,16 +84,30 @@ var15H <- sd(bucksug15$mass[bucksug15$treatment=="H"])^2
 ratio15 <- var15C/var15H
 ratio15
 
+lnvar15C <- sd(bucksug15$lnmass[bucksug15$treatment=="C"])^2
+lnvar15H <- sd(bucksug15$lnmass[bucksug15$treatment=="H"])^2
+lnratio15 <- lnvar15C/lnvar15H
+lnratio15
+
 var16C <- sd(bucksug16$mass[bucksug16$treatment=="C"])^2
 var16H <- sd(bucksug16$mass[bucksug16$treatment=="H"])^2
 ratio16 <- var16C/var16H
 ratio16
+
+lnvar16C <- sd(bucksug16$lnmass[bucksug16$treatment=="C"])^2
+lnvar16H <- sd(bucksug16$lnmass[bucksug16$treatment=="H"])^2
+lnratio16 <- lnvar16C/lnvar16H
+lnratio16
 
 varbothC <- sd(bucksugboth$mass[bucksugboth$treatment=="C"])^2
 varbothH <- sd(bucksugboth$mass[bucksugboth$treatment=="H"])^2
 ratioboth <- varbothC/varbothH
 ratioboth
 
+lnvarbothC <- sd(bucksugboth$lnmass[bucksugboth$treatment=="C"])^2
+lnvarbothH <- sd(bucksugboth$lnmass[bucksugboth$treatment=="H"])^2
+lnratioboth <- lnvarbothC/lnvarbothH
+lnratioboth
 
 # Q-Q plots
 qqnorm(bucksug15$BRIX)
@@ -96,9 +118,15 @@ qqnorm(bucksug15$mass)
 qqnorm(bucksug16$mass)
 qqnorm(bucksugboth$mass)
 
+qqnorm(bucksug15$lnmass)
+qqnorm(bucksug16$lnmass)
+qqnorm(bucksugboth$lnmass)
 
 #Checking spread of key variables
 with(bucksugboth, plot(quad, BRIX,  main = "BRIX by quadrant", xlab = "quadrant", ylab = "BRIX"))
 with(bucksugboth, plot(quad, mass,  main = "Sugar mass by quadrant", xlab = "quadrant", ylab = "mass"))
+with(bucksugboth, plot(quad, lnmass,  main = "ln(sugar mass) by quadrant", xlab = "quadrant", ylab = "ln(mass)"))
+
 with(bucksugboth, plot(date, BRIX,  main = "BRIX by date", xlab = "date", ylab = "BRIX"))
 with(bucksugboth, plot(date, mass,  main = "Sugar mass by date", xlab = "date", ylab = "mass"))
+with(bucksugboth, plot(date, lnmass,  main = "ln(sugar mass) by date", xlab = "date", ylab = "ln(mass)"))
