98ec121abbb64c21062a67fe4861a272f4d23cb4
almccombs <amccombs@iastate.edu>
2017-06-26 20:17:18 -0500

    Added ln(volume) to balsamroot data diagnostics

--- nectar analysis/analysis/Data Exploration/BalsamVolExplr.R
+++ nectar analysis/analysis/Data Exploration/BalsamVolExplr.R
@@ -7,6 +7,10 @@ balsvol15 <- read.csv("nectar analysis/data files/balsvol15.csv", header = T)
 balsvol16 <- read.csv("nectar analysis/data files/balsvol16.csv", header = T)
 balsvolboth <- rbind(balsvol15,balsvol16)
 
+balsvol15$lnvol <- log(balsvol15$volume)
+balsvol16$lnvol <- log(balsvol16$volume)
+balsvolboth$lnvol <- log(balsvolboth$volume)
+
 #Data summaries
 
 summary(balsvol15)
@@ -20,6 +24,10 @@ summarize(group_by(balsvolboth, treatment), meanVol = mean(volume), sdVolume = s
 qplot(balsvol15$volume, binwidth = .025)
 qplot(balsvol16$volume, binwidth = .01)
 qplot(balsvolboth$volume, binwidth = .025)
+
+qplot(balsvol15$lnvol, binwidth = .05)
+qplot(balsvol16$lnvol, binwidth = .05)
+qplot(balsvolboth$lnvol, binwidth = .05)
 
 ggplot(balsvol15, aes(x=treatment, y=volume)) + geom_boxplot() +
   xlab("Treatment") +
@@ -40,15 +48,30 @@ var15H <- sd(balsvol15$volume[balsvol15$treatment=="H"])^2
 ratio15 <- var15H/var15C
 ratio15
 
+lnvar15C <- sd(balsvol15$lnvol[balsvol15$treatment=="C"])^2
+lnvar15H <- sd(balsvol15$lnvol[balsvol15$treatment=="H"])^2
+lnratio15 <- lnvar15H/lnvar15C
+lnratio15
+
 var16C <- sd(balsvol16$volume[balsvol16$treatment=="C"])^2
 var16H <- sd(balsvol16$volume[balsvol16$treatment=="H"])^2
 ratio16 <- var16C/var16H
 ratio16
 
+lnvar16C <- sd(balsvol16$lnvol[balsvol16$treatment=="C"])^2
+lnvar16H <- sd(balsvol16$lnvol[balsvol16$treatment=="H"])^2
+lnratio16 <- lnvar16H/lnvar16C
+lnratio16
+
 varbothC <- sd(balsvolboth$volume[balsvolboth$treatment=="C"])^2
 varbothH <- sd(balsvolboth$volume[balsvolboth$treatment=="H"])^2
 ratioboth <- varbothH/varbothC
 ratioboth
+
+lnvarbothC <- sd(balsvolboth$lnvol[balsvolboth$treatment=="C"])^2
+lnvarbothH <- sd(balsvolboth$lnvol[balsvolboth$treatment=="H"])^2
+lnratioboth <- lnvarbothH/lnvarbothC
+lnratioboth
 
 # Q-Q plots
 
@@ -56,7 +79,15 @@ qqnorm(balsvol15$volume)
 qqnorm(balsvol16$volume)
 qqnorm(balsvolboth$volume)
 
+qqnorm(balsvol15$lnvol)
+qqnorm(balsvol16$lnvol)
+qqnorm(balsvolboth$lnvol)
+
 # Plots of response by variable
   # by date
 with(balsvolboth, plot(date, volume, main = "Volume by date", xlab = "Date", ylab = "Volume"))
 with(balsvolboth, plot(plot, volume, main = "Volume by plot", xlab = "Plot", ylab = "volume"))
+
+with(balsvolboth, plot(date, lnvol, main = "ln(volume) by date", xlab = "Date", ylab = "Volume"))
+with(balsvolboth, plot(plot, lnvol, main = "ln(volume) by plot", xlab = "Plot", ylab = "volume"))
+
