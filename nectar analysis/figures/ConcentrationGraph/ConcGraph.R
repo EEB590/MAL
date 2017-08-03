30babf2d07414e387c54cc5980f6db2f256cb40e
almccombs <amccombs@iastate.edu>
2017-08-02 20:19:06 -0500

    Concentration graph

--- nectar analysis/figures/ConcentrationGraph/ConcGraph.R
+++ nectar analysis/figures/ConcentrationGraph/ConcGraph.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-taxa <- c("Fruit Fly", "Hawkmoth", "Butterfly", "Short-tongue bee", "Stingless bee", "Bumblebee", "Honeybee", "Hummingbird", "Sundbird", "Honeyeater", "Bat")
+taxa <- c("Fruit fly", "Hawkmoth", "Butterfly", "Short-tongue bee", "Stingless bee", "Bumblebee", "Honeybee", "Hummingbird", "Sunbird", "Honeyeater", "Bat")
 mins <- (c(24,34,30,50,45,60,40,30,30,40,60))
 maxs <- (c(24,34,45,65,60,60,45,30,50,50,60))
 
@@ -27,7 +27,7 @@ sdHbuck <- sd(subset(buck15, treatment == "H")$BRIX)
 ggplot(conc, aes(x = taxa, y = max)) +
   geom_linerange(data = conc[c(3,4,5,7,9,10),], mapping = aes(x = taxa, ymin = min, ymax = max), size = 2) +
   geom_pointrange(data = conc[c(1,2,6,8,11),], mapping = aes(x = taxa, y = max, ymin = min, ymax = max), size = 1) +
-  labs(x = "Taxon", y = "Concentration (BRIX)", title = "Optimum Nectar Concentrations") +
+  labs(x = "Taxon", y = "Concentration (BRIX)", title = "Nectar Concentrations, Optimal vs. Observed") +
   theme(axis.text.x = element_text(angle=30, hjust=1, vjust=1)) +
   geom_hline(yintercept = meanHbals, col = "red") +
   geom_hline(yintercept = meanCbals, col = "green") +
