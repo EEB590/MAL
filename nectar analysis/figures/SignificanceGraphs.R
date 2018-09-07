27e35eb4513c11015cc86ff7112dca7806fb2e3d
almccombs <amccombs@iastate.edu>
2018-09-06 20:02:28 -0500

    Update Figure 1 with new presence-absence graph

--- nectar analysis/figures/SignificanceGraphs.R
+++ nectar analysis/figures/SignificanceGraphs.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(reshape)
 library(gridExtra)
 
 setwd("D:/Iowa State University/Debinski Lab/Nectar data/MAL")
@@ -44,14 +45,19 @@ buck15brix <- ggplot(bucksug15, aes(x=treatment, y=BRIX)) + geom_boxplot() +
   xlab("Treatment") +
   ylab("Nectar BRIX") + ggtitle("Buckwheat BRIX 2015")
 
-necpa <- ggplot(balspa, aes(x = datechr, fill = necpres)) +
-  theme(axis.text.x = element_text(angle=20, hjust=1, vjust=1)) +
+prestable <- table(balspa)
+presdf <- melt(prestable); rm(prestable)
+totalsdf <- summarize(group_by(presdf, datechr, treatment), sum(value))
+presdf <- merge(presdf, totalsdf); rm(totalsdf)
+presdf <- presdf[which(presdf$necpres == 1),]
+presdf$percent <- presdf$value/presdf$`sum(value)` * 100
+levels(presdf$datechr) <- c(levels(presdf$datechr), "June 04", "June 05", "June 07", "June 09")
+
+necpres <- ggplot(presdf, aes(x = datechr, y = percent, color = treatment, group = treatment)) +
+  geom_line() + theme(axis.text.x = element_text(angle=20, hjust=1, vjust=1)) +
   theme(axis.title.x=element_blank()) +
-  geom_bar(position = "fill") +
-  scale_fill_manual(values=c("gray30", "grey70"), labels=c("Absent", "Present")) +
-  labs(y = "Percent", title = "Nectar Presence/Absence", subtitle = "by treatment, Balsamroot 2015") +
-  guides(fill=guide_legend(title=NULL, reverse = TRUE)) +
-  facet_grid(treatment~.)
+  labs(y = "Percent", title = "Percent of Flowers that Contain Nectar",
+       subtitle = "Balsamroot 2015", color = "Treatment")
 
 flowers <- read.csv("nectar analysis/data files/raw data/Balsamroot phenology/TotalFlowersPerPlant.csv", header = T, as.is = T)
 names(flowers)[4:5] <- c("count15", "count16")
@@ -63,7 +69,7 @@ g2 <- ggplot(flowers, aes(x = treatment, y = count16)) + geom_boxplot() +
   ylim(0, 50) + labs(title = "2016", x = NULL, y = NULL)
 totflow <- grid.arrange(g1, g2, ncol = 2, top = grid::textGrob("Total # of Flowers, Balsamroot",x=0.05,hjust=0), bottom = "Treatment")
 
-grid.arrange(bals15brix, bals16mass, buck15brix, buck15volln, necpa, totflow, ncol=2)
+grid.arrange(bals15brix, bals16mass, buck15brix, buck15volln, necpres, totflow, ncol=2)
 
 
 ###  Old code
@@ -74,5 +80,15 @@ grid.arrange(bals15brix, bals16mass, buck15brix, buck15volln, necpa, totflow, nc
 
 #totflow <- ggplot(flowers, aes(x = total)) + geom_histogram(binwidth = 1) + facet_grid(treatment~.) +
 #  labs(title = "Total Flowers", subtitle = "by treatment, Balsamroot 2015 + 2016", x = "Total Flowers", y = "Count of Plants") 
+
+#necpa <- ggplot(balspa, aes(x = datechr, fill = necpres)) +
+#  theme(axis.text.x = element_text(angle=20, hjust=1, vjust=1)) +
+#  theme(axis.title.x=element_blank()) +
+#  geom_bar(position = "fill") +
+#  scale_fill_manual(values=c("gray30", "grey70"), labels=c("Absent", "Present")) +
+#  labs(y = "Percent", title = "Nectar Presence/Absence", subtitle = "by treatment, Balsamroot 2015") +
+#  guides(fill=guide_legend(title=NULL, reverse = TRUE)) +
+#  facet_grid(treatment~.)
+
 
 
